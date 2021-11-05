d63f9e465fc3e491ce7247620b65eb7a6ea5f412
朱庆积 <zhuqingji@zhuqingjideMacBook-Pro.local>
2021-11-05 23:37:14 +0800

    朱庆积第四次提交

---
+++ test.js
@@ -0,0 +1,12 @@
+
+let obj = {
+    name:"Baz",age:18,gender:'male'};
+  let obj1=obj;
+  let obj2=obj;   
+  let obj3=obj;
+  let arr =[];
+  arr.push(obj1,obj2,obj3);
+  console.table(arr);
+
+  
+  
